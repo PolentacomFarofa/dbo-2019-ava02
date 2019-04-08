2fa9c41a858b3fb7ec410cb3ba6144584c50223b
Farofa <41998617+PolentacomFarofa@users.noreply.github.com>
2019-04-07 22:25:30 -0300

    Add files via upload

---
+++ netflix.js
@@ -0,0 +1,185 @@
+// netflix.js // 0.5 pts
+console.log('netflix.js');
+
+const Netflix = { // deve sublinhar como erro, altere para const para consertar
+    series: [
+        {
+           title: 'Black Mirror',
+           creators: 'Charlie Brooker',
+           classificacao: 16,
+           year: 2017,
+           seasons: [
+               {
+                title: 'Temporada 1',
+                episodes: [
+                       {
+                           title: 'The National Anthem',
+                           sinopse: 'O primeiro-ministro Michael Callow enfrenta um chocante dilema quando a amada princesa Susannah é raptada.',
+                           duracao: 44
+                       },
+                       {
+                           title: 'Fifteen Million Merits',
+                           sinopse: 'Após fracassar em um concurso como cantora, uma mulher tem que escolher entre praticar atos humilhantes ou voltar a viver como escrava.',
+                           duracao: 61
+                       },
+                       {
+                           title: 'The Entire History of You',
+                           sinopse: 'No futuro, todos tem acesso a um implante de memoria que grava tudo o que os seres humanos fazem, veem ou ouvem.',
+                           duracao: 48
+                       }
+                   ]
+
+               },
+               {
+                   title: 'Temporada 2',
+                   episodes: [
+                       {
+                           title: 'Be Right Back',
+                           sinopse: 'Martha reencontra o seu falecido amante através de um novo serviço que permite que as pessoas mantenham contato com os mortos',
+                           duracao: 48
+                       },
+                       {
+                           title: 'White Bear',
+                           sinopse: 'Victoria acorda e não se lembra de nada sobre sua vida. Todos que encontra se recusam a falar com ela',
+                           duracao: 42
+                       },
+                       {
+                           title: 'The waldo moment',
+                           sinopse: 'Um comediante fracassado que dubla um urso animado se envolve em uma trama política quando seu personagem se candidata a um cargo político',
+                           duracao: 43
+                       },
+                       {
+                           title: 'natal',
+                           sinopse: 'Dois homens em uma área gélida e remota contam três histórias sobre descontroles tecnológicos ocorridas em época de Natal',
+                           duracao: 73
+                       }
+                   ]
+               }
+           ]
+        },
+        {
+            title: 'Love, Death & Robots',
+            creators: 'Tim Miller, David Fincher',
+            classificacao: 18,
+            year:2019,
+            seasons: [
+                {
+                    title: 'Temporada 1',
+                    episodes: [
+                        {
+                            title: 'A vantagem de Sonnie',
+                            sinopse: 'No submundo das batalhas de feras, Sonnie é invencivel. Mas para continuar assim, precisa manter vantagem sobre os outros.',
+                            duracao: 17
+                        },
+                        {
+                            title: 'Os três robôs',
+                            sinopse: 'Muito tempo depois do fim da humanidade, três robôs fazem uma excursão por uma cidade pós-apocalíptica.',
+                            duracao: 11
+                        },
+                        {
+                            title: 'A testemunha',
+                            sinopse: 'Após testemunhar um crime brutal, uma dançarina embarca em um frenético jogo de gato e rato com o assassino pelas ruas de uma cidade surreal.',
+                            duracao: 12
+                        },
+                        {
+                            title: 'Proteção contra alienígenas',
+                            sinopse: 'Uma comunidade de fazendeiros usa armaduras robóticas caseiras para defender suas famílias de uma invasão alienígena.',
+                            duracao: 17
+                        },
+                        {
+                            title: 'Sugador de Almas',
+                            sinopse: 'Libertado por uma escavação arqueológica, um demônio sedento de sangue enfrenta uma equipe de mercenários equipados com... gatos?!.',
+                            duracao: 13
+                        },
+                        {
+                            title: 'Quando o iogurte assumiu o controle',
+                            sinopse: 'Acidentalmente, cientistas criam um iogurte superinteligente, e agora o lactínio só pensa em uma coisa: dominar o mundo.',
+                            duracao: 6
+                        },
+                        {
+                            title: 'Para além da fenda de Áquila',
+                            sinopse: 'Ao acordar após viajar fora de curso por anos-luz, a tripulação de uma nave tenta compreender em que local do universo se encontra.',
+                            duracao: 16
+                        },
+                        {
+                            title: 'Boa caçada',
+                            sinopse: 'O filho de um caçador de espíritos faz amizade com uma criatura metamórfica.',
+                            duracao: 17
+                        },
+                        {
+                            title: 'O lixão',
+                            sinopse: 'O lixão é o lugar que ele chama de lar, e não vai ser qualquer fanfarrão da cidade que irá tirá-lo de lá.',
+                            duracao: 10
+                        },
+                        {
+                            title: 'Metamorfos',
+                            sinopse: 'No Afeganistão, dois fuzileiros com poderes sobrenaturais enfrentam uma ameaça com a qual tem muito em comum.',
+                            duracao: 16
+                        },
+                        {
+                            title: 'Ajudinha',
+                            sinopse: 'À deriva no espaço, uma astronauta precisa escolher entre sacrificar uma parte de seu corpo ou perder a vida.',
+                            duracao: 10
+                        },
+                        {
+                            title: 'Noite de pescaria',
+                            sinopse: 'Depois que seu carro quebra no meio do deserto, dois vendedores embarcam em uma viagem psicodélica ao início dos tempos.',
+                            duracao:10
+                        },
+                        {
+                            title: '13, número da sorte',
+                            sinopse: 'Ninguém queria voar com a aeronave treze depois de sua tripulação ser dizimada duas vezes. Acontece que pilotos novatos não tem escolha.',
+                            duracao: 14
+                        },
+                        {
+                            title: 'Zima Blue',
+                            sinopse: 'O famoso artista Zima fala sobre seu passado misterioso e sua ascenção ao estrelato antes de revelar seu trabalho final.',
+                            duracao: 10
+                        },
+                        {
+                            title: 'Ponto cego',
+                            sinopse: 'Uma gangue de ciborgues faz um ataque relâmpago a um comboio armado.',
+                            duracao: 8
+                        },
+                        {
+                            title: 'Era do gelo',
+                            sinopse: 'Um jovem casal descobre uma civilização perdida dentro do freezer de seu novo apartamento.',
+                            duracao: 10
+                        },
+                        {
+                            title: 'Histórias alternativas',
+                            sinopse: 'Quer ver hitler morrer das formas mais cômicas possíveis? Agora você pode. Bem vindo à multiversidade.',
+                            duracao: 7
+                        },
+                        {
+                            title: 'A guerra secreta',
+                            sinopse: 'Unidades de elite do exército russo enfrentam forças malignas nas remotas florestas da Sibéria',
+                            duracao: 16
+                        }
+                    ]
+                }
+            ]
+        }
+    ]
+};
+
+// title of the first TV serie
+console.log(Netflix['series'][0]['title']); // any title
+// there is a first season with at least one episode, must print true
+console.log(Netflix['series'][0]['seasons'][0]['episodes'].length > 0);
+console.log(Netflix[
+    'series'][0][
+    'seasons'][0][
+    'episodes'][0][
+    'title'] !== undefined); // must be true
+
+// Add more test cases which should cover
+// the assessment specification:
+console.log(Netflix['series'][1]['seasons'][0]['episodes'][5]['title']) //Quando o iogurte assumiu o controle
+console.log(Netflix['series'][1]['seasons'][0]['episodes'][5]['sinopse'])
+console.log(Netflix['series'][0]['seasons'][1]['episodes'][2]['title'])
+console.log(Netflix['series'][0]['seasons'][1]['episodes'][2]['sinopse'])
+console.log(Netflix['series'][0]['year']) //2017
+console.log(Netflix['series'][1]['year']) //2019
+console.log(Netflix['series'][1]['creators'])
+console.log(Netflix['series'][0]['creators'])
